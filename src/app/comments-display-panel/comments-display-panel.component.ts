8ac3c9c2fed5ade764e49f31a4e8b6d6746cc111
Manvir <manvirheer007@gmail.com>
2021-12-02 20:28:06 -0800

    Made changes to comments section to have it working properly

--- src/app/comments-display-panel/comments-display-panel.component.ts
+++ src/app/comments-display-panel/comments-display-panel.component.ts
@@ -13,15 +13,18 @@ export class CommentsDisplayPanelComponent implements OnInit {
 
   @Input() workItemNumber;
   @ViewChild('listRef') listRef!: ElementRef;
-   list = document.querySelector('#list');
+  @ViewChild('commentsRef') commentsRef!: ElementRef;
+  @ViewChild('enterRef') enterRef!: ElementRef;
+  @ViewChild('commentBox') commentBox!: ElementRef;
+  list = document.querySelector('#list');
   ngOnChanges(changes) {
-    if(this.listRef) {
-    //this.list.innerHTML = ''
+    if (this.listRef) {
+      //this.list.innerHTML = ''
     }
-    if(this.workItemNumber){
+    if (this.workItemNumber) {
       this.consoleTest();
-      this.getComments()
-
+      this.getComments();
+      this.renderer.setStyle(this.commentBox.nativeElement, 'display', 'block');
     }
   }
 
@@ -37,36 +40,88 @@ export class CommentsDisplayPanelComponent implements OnInit {
         "Content-Type": 'application/json'
       }
     }).subscribe((res) => {
-      this.response = res
-      console.log(this.response)
-      if (this.response)
-        this.response!.comments.forEach((comment: any) => {
+      this.response = res;
+      //Remove the previous list of comments and extra modals
+      Array.from(this.listRef.nativeElement.children).forEach(child => {
+        console.log('children.length=' + this.listRef.nativeElement.children.length);
+        this.renderer.removeChild(this.listRef.nativeElement, child);
+      });
 
+      //If there are no commetns for that ticket then show unable to found any previous comments. 
+      if (this.response.comments.length == 0) {
+        var noCommentFoundModal = this.renderer.createElement('div');
+        noCommentFoundModal.className = 'text-lg font-light font-customPro p-6 text-white text-center rounded-lg border-2 bg-gray-400';
+        noCommentFoundModal.textContent = "Unable to found any previous comments";
+        this.renderer.appendChild(this.listRef.nativeElement, noCommentFoundModal);
+      }
+      //for the comments found, show the list
+      else {
+        this.response!.comments.forEach((comment: any) => {
           var outerDiv = this.renderer.createElement('div');
-          outerDiv.className = 'bg-white  mt-2 rounded-lg p-3 border-2 min-w-full';
           var innerDivLook = this.renderer.createElement('div');
-          innerDivLook.className = 'flex items-center space-x-6 mb-4 w-full'
-          var innerDivLookImg = this.renderer.createElement('img')
-          innerDivLookImg.className = 'h-5 w-5 object-cover object-center rounded-full'
-          innerDivLookImg.src = 'https://cdn-icons-png.flaticon.com/512/747/747376.png'
+          var innerDivLookImg = this.renderer.createElement('img');
           var innerDivLookDiv = this.renderer.createElement('div');
-          var innerDivLookDivP = this.renderer.createElement('p')
-          innerDivLookDivP.className = 'text-lg text-gray-700 font-normal mb-1'
-          innerDivLookDivP.textContent = comment.createdBy.displayName
-          innerDivLookDiv.appendChild(innerDivLookDivP);
-          innerDivLook.appendChild(innerDivLookImg)
-          innerDivLook.appendChild(innerDivLookDiv)
-          outerDiv.appendChild(innerDivLook)
+          var innerDivLookDivP = this.renderer.createElement('p');
           var innerDivContent = this.renderer.createElement('div');
           var innerDivContentP = this.renderer.createElement('p');
-          innerDivContentP.textContent = (comment.text.replace(/<[^>]*>/g, '').replace(/&nbsp;/g, ' '))
+          outerDiv.className = 'bg-white  mt-2 rounded-lg p-3 border-2 min-w-full';
+          innerDivLook.className = 'flex items-center space-x-6 mb-4 w-full';
+          innerDivLookImg.className = 'h-5 w-5 object-cover object-center rounded-full';
+          innerDivLookDivP.className = 'text-lg text-gray-700 font-normal mb-1';
+          innerDivLookImg.src = 'https://cdn-icons-png.flaticon.com/512/747/747376.png';
+          innerDivLookDivP.textContent = comment.createdBy.displayName;
+          innerDivContentP.textContent = (comment.text.replace(/<[^>]*>/g, '').replace(/&nbsp;/g, ' '));
+          innerDivLookDiv.appendChild(innerDivLookDivP);
+          innerDivLook.appendChild(innerDivLookImg);
+          innerDivLook.appendChild(innerDivLookDiv);
+          outerDiv.appendChild(innerDivLook);
           innerDivContent.appendChild(innerDivContentP);
           outerDiv.appendChild(innerDivContent);
-
-
-          this.renderer.appendChild(this.listRef.nativeElement, outerDiv)
+          this.renderer.appendChild(this.listRef.nativeElement, outerDiv);
         })
+      };
     })
+  }
+
+  addComment() {
+    Array.from(this.listRef.nativeElement.children).forEach(child => {
+      if ((child as HTMLElement).textContent == 'Unable to found any previous comments')
+        this.renderer.removeChild(this.listRef.nativeElement, child);
+    });
+    this.http.post<any>(`https://dev.azure.com/saasberry/SaaSberry%20Innovation%20Lab/_apis/wit/workItems/${(this.workItemNumber)}/comments?api-version=6.0-preview.3`,
+      {
+        "text": this.commentsRef.nativeElement.value
+      },
+      {
+        headers: {
+          "Authorization": "Basic OjJuc2VwM2V1b211cWVxYWJqcW1rdnVuMmtqbGc1ZHByMzduMnZ1NTRpcGV4M2UycDRuaXE=",
+          "Content-Type": 'application/json'
+        }
+      }).subscribe(res => {
+        var outerDiv = this.renderer.createElement('div');
+        outerDiv.className = 'bg-white  mt-2 rounded-lg p-3 border-2 min-w-full';
+        var innerDivLook = this.renderer.createElement('div');
+        innerDivLook.className = 'flex items-center space-x-6 mb-4 w-full'
+        var innerDivLookImg = this.renderer.createElement('img')
+        innerDivLookImg.className = 'h-5 w-5 object-cover object-center rounded-full'
+        innerDivLookImg.src = 'https://cdn-icons-png.flaticon.com/512/747/747376.png'
+        var innerDivLookDiv = this.renderer.createElement('div');
+        var innerDivLookDivP = this.renderer.createElement('p')
+        innerDivLookDivP.className = 'text-lg text-gray-700 font-normal mb-1'
+        innerDivLookDivP.textContent = "User"
+        innerDivLookDiv.appendChild(innerDivLookDivP);
+        innerDivLook.appendChild(innerDivLookImg)
+        innerDivLook.appendChild(innerDivLookDiv)
+        outerDiv.appendChild(innerDivLook)
+        var innerDivContent = this.renderer.createElement('div');
+        var innerDivContentP = this.renderer.createElement('p');
+        innerDivContentP.textContent = this.commentsRef.nativeElement.value
+        innerDivContent.appendChild(innerDivContentP);
+        outerDiv.appendChild(innerDivContent);
+        this.renderer.appendChild(this.listRef.nativeElement, outerDiv)
+        console.log(res);
+        this.commentsRef.nativeElement.value = ""
+      })
 
   }
   ngOnInit(): void {
